778b2cf6cb28e39b77b7ca0602125c6c68e5915d
Ziad-Bahaa2006 <ziadbaha797@gmail.com>
2025-05-17 16:00:33 +0300

    profile  page & and the latest version of home page

--- home_page/script.js
+++ home_page/script.js
@@ -1,22 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(){
-const profileIcon = document.querySelector(".profile-icon");
+//this code become unsed
 
-    if (profileIcon) {
-        profileIcon.addEventListener("click", function(){
-            this.classList.toggle("active");
-            document.querySelectorAll(".profile-icon").forEach(icon =>{
-                if (icon !== this ) icon.classList.remove("active");
-            });
-        });
-    }
+// document.addEventListener("DOMContentLoaded", function(){
+// const profileIcon = document.querySelector(".profile-icon");
 
-    document.addEventListener("click", function(e) {
-        const dropdown = document.querySelector(".menu");
-        const icon = document.querySelector(".profile-icon");
+//     if (profileIcon) {
+//         profileIcon.addEventListener("click", function(){
+//             this.classList.toggle("active");
+//             document.querySelectorAll(".profile-icon").forEach(icon =>{
+//                 if (icon !== this ) icon.classList.remove("active");
+//             });
+//         });
+//     }
 
-        if (!e.target.closest(".profile-icon") && !e.target.closest(".menu")) {
-            if (icon) icon.classList.remove("active");
-        }
-    });
-});
+//     document.addEventListener("click", function(e) {
+//         const dropdown = document.querySelector(".menu");
+//         const icon = document.querySelector(".profile-icon");
+
+//         if (!e.target.closest(".profile-icon") && !e.target.closest(".menu")) {
+//             if (icon) icon.classList.remove("active");
+//         }
+//     });
+// });
 
